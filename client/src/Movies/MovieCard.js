b32a2c9cd4289256681896d2f7a7b52eb575fd14
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-10-15 12:41:56 -0500

    Updated all cards and links

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({ title, director, metascore }) => {
+  return (
+    <div className="movie-card">
+    <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+    </div>
+  );
 };
 
 export default MovieCard;
